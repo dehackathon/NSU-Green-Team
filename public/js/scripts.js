fa67d621c73cb14dd9568aa7846923f9a442d186
Kotut <lynwazim@yahoo.com>
2015-02-12 22:54:02 -0500

    added progress bar

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -41,6 +41,16 @@ $(document).ready(function() {
 
 });
 
+$(function() {
+	$('input').on('click', function() {
+		var emptyValue = 0;
+		$('input:checked').each(function() {
+			emptyValue += parseInt($(this).val());
+		});
+		$('.progress-bar').css('width', emptyValue + '%').attr('aria-valuenow', emptyValue);
+	});
+});
+
 
 
 $(function() {
